5e7df661676830ab707c3776f0c15d8565938bc0
Kalash <kalashjain513@gmail.com>
2024-11-20 21:16:26 +0530

    tweaking ros2 inference node to work with the ros bag

--- ROS2_node_development_ws/src/detection_and_segmentation/detection_and_segmentation/object_detection_node.py
+++ ROS2_node_development_ws/src/detection_and_segmentation/detection_and_segmentation/object_detection_node.py
@@ -5,6 +5,8 @@ from cv_bridge import CvBridge
 import cv2
 from ultralytics import YOLO
 
+from rclpy.qos import QoSProfile, ReliabilityPolicy
+
 
 class ObjectDetectionNode(Node):
     def __init__(self):
@@ -12,9 +14,12 @@ class ObjectDetectionNode(Node):
 
         self.model_ = YOLO("./src/detection_and_segmentation/models/best.pt") # the fine-tuned YOLOv11 model on our dataset
         self.bridge_ = CvBridge()
-
-        self.subscriber_ = self.create_subscription(Image, "/camera/image_raw", self.callback, 10)
         
+        self.get_logger().info("Now listening to your topic!")
+
+        qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT, depth=10)
+        self.subscriber_ = self.create_subscription(Image, "/robot1/zed2i/left/image_rect_color", self.callback, qos_profile)
+
         # for depth topics
         # self.subscriber_ = self.create_subscription(Image, "/camera/depth/image_raw", self.callback, 10)
 
@@ -36,13 +41,11 @@ class ObjectDetectionNode(Node):
                 conf = round(float(confidence)*100, 3)
 
                 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(image, "Pallet: "+str(conf), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                
+                cv2.putText(image, "Pallet: " + str(conf), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
             
         cv2.imshow("Object Detection", image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
-            rclpy.shutdown()
 
 
 def main():
